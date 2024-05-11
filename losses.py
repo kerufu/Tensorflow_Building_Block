08cf6e094647a66b2ee746ad8b3dc033311d40b4
kerufu <45701634+kerufu@users.noreply.github.com>
2024-05-11 02:50:08 -0700

    tested

--- losses.py
+++ losses.py
@@ -11,8 +11,8 @@ class GradientPenalty():
         x_mix = epsilon * tf.cast(x_true, tf.float32) + (1 - epsilon) * x_fake
         with tf.GradientTape() as tape:
             tape.watch(x_mix)
-            discriminaotr_score_mix = discriminator(x_mix)
-        gradients = tape.gradient(discriminaotr_score_mix, x_mix)
+            discriminator_score_mix = discriminator(x_mix)
+        gradients = tape.gradient(discriminator_score_mix, x_mix)
         gradient_mean = tf.sqrt(tf.reduce_sum(gradients**2, axis=list(range(1, len(gradients.shape)))))
         gradient_penalty = tf.reduce_mean((gradient_mean-1.0)**2)
         return gradient_penalty
